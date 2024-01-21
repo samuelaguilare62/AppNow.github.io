05f210b66548ddbbdea412874a5112dff3b6b844
samuelaguilare62 <67772951+samuelaguilare62@users.noreply.github.com>
2024-01-20 18:11:09 -0600

    Add files via upload
    Signed-off-by: samuelaguilare62 <67772951+samuelaguilare62@users.noreply.github.com>

---
+++ inicio.js
@@ -0,0 +1,129 @@
+const faqData = {
+  "Hola": "Hola ! ¿En que podemos ayudarte?.",
+  "¿Las apps y juegos son libres de virus?": "Sí, la descarga de WebAppify es libre de virus. Siempre checamos los link de las aplicaciones o juegos y verificamos que ka app o juego esten libres de virus y esto con el fin de garantizar la seguridad.",
+  "¿Es necesario resgistrarse o crear cuenta para ingresar a webappify?": "No,no es necesario ya que por el momento no se piensa añadir o implementar el formulario para registrarse ,pero si se esta tomando en cuenta para próximas actualizaciones de la pagina",
+  "¿WebAppiFy esta disponible para dispositivos moviles y computadoras?": "Sí, WebAppiFy está disponible tanto para dispositivos móviles como para computadoras. Puedes acceder a nuestra plataforma y disfrutar de todas las aplicaciones y juegos desde tu teléfono móvil o tablet, así como desde tu computadora personal. Esto significa que puedes llevar la diversión y la conveniencia de WebAppiFy contigo donde quiera que vayas, independientemente del dispositivo que utilices",
+  "¿ofrecen actualizaciones regulares en las aplicaciones  o juegos ?": "En WebAppiFy, nos esforzamos por mantener nuestras aplicaciones y juegos actualizados de manera regular para ofrecer la mejor experiencia posible a nuestros usuarios. Realizamos actualizaciones de la página periódicamente y trabajamos diligentemente para garantizar que todas las aplicaciones y juegos estén siempre al día. Nuestra política es realizar actualizaciones al menos cada semana o incluso con mayor frecuencia si es necesario. Esto nos permite proporcionarte las últimas versiones de las aplicaciones y juegos para que siempre disfrutes de contenido fresco y actualizado",
+  "¿Puedo encontrar aplicacionesy juegos populares?": "Sí, en WebAppiFy, hacemos todo lo posible para ofrecer una amplia selección de aplicaciones y juegos populares. Sin embargo, debido a que somos una página web en crecimiento, no podemos garantizar que estén disponibles todas las aplicaciones y juegos populares. Estamos trabajando constantemente para expandir nuestro catálogo y brindarte más opciones, pero te recomendamos explorar nuestra plataforma para descubrir las joyas ocultas y las últimas tendencias que ofrecemos. ¡Quién sabe, podrías encontrar nuevas aplicaciones y juegos que te sorprendan gratamente!.",
+  "¿Existe algun limite en la cantidad de aplicaciones y juegos que puedo descargar ?": "No, en WebAppiFy no imponemos ningún límite en la cantidad de aplicaciones y juegos que puedes descargar. Nuestra plataforma funciona mediante enlaces directos para la descarga de aplicaciones, lo que significa que puedes acceder a tantos títulos como desees. El único límite que podría afectar tu capacidad de descarga es el espacio disponible en tu dispositivo. Así que, si tienes suficiente espacio en tu dispositivo, puedes descargar y disfrutar de todas las aplicaciones y juegos que quieras desde WebAppiFy sin restricciones",
+  "¿La aplicación es gratuita o tiene algún costo?" :"¡Bienvenido! Todas nuestras aplicaciones y juegos son totalmente gratuitos. Siéntete libre de descargar cualquier aplicación o juego que desees. Además, puedes consultar las preguntas que puedes hacer desde las opciones que tienes abajo. ¡Disfruta de la experiencia y no dudes en hacer más preguntas si necesitas ayuda!",
+  "¿Cuáles son los requisitos del sistema para instalar esta aplicación/juego?" : "Los requisitos del sistema para instalar nuestras aplicaciones y juegos son mínimos. Puedes descargarlos de forma sencilla haciendo clic en el botón de descarga correspondiente. Una vez hagas clic, serás redirigido a otro enlace donde encontrarás un botón azul con el nombre de la aplicación o juego. Solo necesitas dar clic en ese botón azul, y la descarga comenzará automáticamente. ¡Esperamos que disfrutes de la aplicación o juego! Si tienes alguna otra pregunta, no dudes en preguntar.",
+  "¿Cómo instalo la aplicación en mi dispositivo móvil?" : "¡CLARO! Una vez que hayas descargado la aplicación o juego, sigue estos sencillos pasos para instalarlo en tu dispositivo 1• Dirigete a los tres puntos en la esquina superior derecha de tu navegador al hacerlo, se desplegarán varias opciones. 2• Busca la opción Descarga o Descargas y haz clic en ella. 3• Encontraras el archivo de la aplicacion o juego que acabas de descargar. 4• Haz clic en el archivo APK de tu juego o aplicación. 5• Aparecera una ventana que te preguntará si deseas instalar la aplicación. Haz clic en Aceptar y espera a que se complete la descarga. 6• Ten en cuenta que es posible que necesites dar permisos a Origenes desconocidos para poder descargar la aplicación. Esta solicitud se mostrará en una pequeña ventana, indicándote que debes habilitar esta opción para permitir la instalación de aplicaciones de fuentes no verificadas. Listo. Ahora deberías tener la aplicación o juego instalado en tu dispositivo. No dudes en hacernos más preguntas si necesitas más ayuda",
+  "¿Cómo actualizo una aplicación a la última versión?" : "Para actualizar la aplicación o juego, simplemente regresa a nuestra página y asegúrate de que esté disponible la última versión. Si hay una actualización, vuelve a descargarla siguiendo el mismo procedimiento que utilizaste inicialmente. ¡Así te asegurarás de tener siempre la versión más reciente y disfrutar de las últimas características y mejoras!",
+  "¿Qué medidas de seguridad se toman para proteger los datos del usuario?" : "En nuestra página, queremos asegurarte que respetamos tu privacidad. No recopilamos información personal ni contraseñas de los usuarios. Puedes estar tranquilo sabiendo que tu experiencia de descarga es segura y sin comprometer tus datos. Si tienes alguna pregunta adicional sobre la seguridad o privacidad, no dudes en consultarlo conmigo. ¡Estoy aquí para ayudarte!",
+  "¿Cómo puedo contactar al soporte técnico si tengo problemas?" : "Para obtener asistencia y contactar con nuestro soporte técnico, te invitamos a unirte a nuestro grupo de Discord. Nuestro equipo estará encantado de ayudarte con cualquier pregunta o problema que puedas tener. Simplemente sigue este enlace para unirte a nuestro servidor Discord: [https://discord.gg/Qn9FutnQ]. ¡Esperamos poder ayudarte pronto!",
+  "Políticas de privacidad y seguridad de WebAppiFy" : "Recopilación de Datos:<br> 1• Sin Recopilación de Datos Personales:<br> En WebAppiFy, no recopilamos información personal identificable, como nombres, direcciones de correo electrónico o contraseñas.<br> Uso de la Información:<br> 1• Ningún Uso de Datos Personales: Dado que no recopilamos datos personales, no utilizamos ninguna información de este tipo.<br> Seguridad:<br> Protección de Datos:<br> Aunque no recopilamos datos personales, implementamos medidas de seguridad para proteger cualquier información técnica y garantizar la seguridad de la plataforma.<br> Derechos de Privacidad:<br> Privacidad Garantizada:<br> Tu privacidad está garantizada ya que no recopilamos datos personales.<br> Cookies y Tecnologías Similares:<br> Uso de Cookies Limitado:<br> Utilizamos cookies de sesión y cookies técnicas que son necesarias para el funcionamiento básico del sitio web. Estas cookies no recopilan información personal y se eliminan cuando cierras tu navegador.<br> Cambios en la Política de Privacidad:<br> Esta política de privacidad se mantiene constante, ya que no se recopilan datos personales."
+
+  // Agrega más preguntas y respuestas según sea necesario
+};
+
+/*
+function toggleChatPopup() {
+  var chatPopup = document.getElementById("chatPopup");
+  chatPopup.style.display = chatPopup.style.display === "block" ? "none" : "block";
+}
+
+function selectOption(question) {
+  // Simula la selección de una opción haciendo clic automáticamente
+  document.getElementById("userInput").value = question;
+  sendUserMessage();
+}
+
+function showAllQuestions() {
+  var chatContent = document.getElementById("chatContent");
+
+  // Filtrar preguntas que contienen signos de interrogación
+  var filteredQuestions = Object.keys(faqData).filter(question => question.includes('?'))
+
+  // Agregar el mensaje y la lista de preguntas al contenido del chat
+  chatContent.innerHTML += `<div class="user-message">Dame una lista de todas las preguntas frecuentes</div>`;
+  chatContent.innerHTML += `<div class="bot-message">Claro. Aqui tienes una lista de las preguntas frecuentes:<br>${filteredQuestions.join('<br>')}</div>`;
+
+  // Desplazar el scroll para mostrar el nuevo mensaje
+  chatContent.scrollTop = chatContent.scrollHeight;
+}
+
+function normalizeString(str) {
+  // Función para normalizar una cadena eliminando signos de interrogación, convirtiéndola a minúsculas y eliminando espacios al principio y al final
+  return str.replace(/\?/g, '').toLowerCase().trim();
+}
+
+function sendUserMessage() {
+  var userInput = document.getElementById("userInput").value;
+  var chatContent = document.getElementById("chatContent");
+
+  // Normalizar la entrada del usuario para comparación
+  var normalizedUserInput = normalizeString(userInput);
+
+  // Buscar una respuesta en las preguntas frecuentes
+  var matchedQuestion = Object.keys(faqData).find(question => normalizeString(question).includes(normalizedUserInput));
+
+  // Agregar el mensaje del usuario y la respuesta al contenido del chat
+  chatContent.innerHTML += `<div class="user-message">${userInput}</div>`;
+  chatContent.innerHTML += matchedQuestion ?
+    `<div class="bot-message">${faqData[matchedQuestion]}</div>` :
+    `<div class="bot-message">Lo siento, no entiendo esa pregunta. ¿Puedo ayudarte con algo más?</div>`;
+
+  // Limpiar el campo de entrada del usuario
+  document.getElementById("userInput").value = "";
+
+  // Desplazar el scroll para mostrar el nuevo mensaje
+  chatContent.scrollTop = chatContent.scrollHeight;
+}*/
+
+
+
+function toggleChatPopup() {
+  var chatPopup = document.getElementById("chatPopup");
+  chatPopup.style.display = chatPopup.style.display === "block" ? "none" : "block";
+}
+
+function selectOption(question) {
+  document.getElementById("userInput").value = question;
+  sendUserMessage();
+}
+
+function showAllQuestions() {
+  var chatContent = document.getElementById("chatContent");
+
+  var filteredQuestions = Object.keys(faqData).filter(question => question.includes('?'))
+
+  chatContent.innerHTML += `<div class="user-message">¡Claro! aqui tienes una lista de todas las preguntas frecuentes.</div>`;
+
+  // Mostrar la lista de preguntas después de la pregunta del usuario
+  chatContent.innerHTML += `<div class="user-message">${filteredQuestions.join('<br>')}</div>`;
+  chatContent.scrollTop = chatContent.scrollHeight;
+}
+
+function normalizeString(str) {
+  return str.replace(/\?/g, '').toLowerCase().trim();
+}
+
+function sendUserMessage() {
+  var userInput = document.getElementById("userInput").value;
+  var chatContent = document.getElementById("chatContent");
+
+  // Mostrar la pregunta del usuario
+  chatContent.innerHTML += `<div class="user-message">${userInput}</div>`;
+
+  // Mostrar el mensaje de "Escribiendo..." después de la pregunta
+  var typingMessage = document.createElement("div");
+  typingMessage.className = "bot-message typing-message";
+  typingMessage.textContent = "Escribiendo...";
+  chatContent.appendChild(typingMessage);
+
+  setTimeout(function () {
+    // Mostrar la respuesta del bot después de "Escribiendo..."
+    var normalizedUserInput = normalizeString(userInput);
+    var matchedQuestion = Object.keys(faqData).find(question => normalizeString(question).includes(normalizedUserInput));
+    var botResponse = matchedQuestion ?
+      `<div class="bot-message">${faqData[matchedQuestion]}</div>` :
+      `<div class="bot-message">Lo siento, no entiendo esa pregunta. ¿Puedo ayudarte con algo más?</div>`;
+
+    // Reemplazar el mensaje de "Escribiendo..." con la respuesta del bot
+    chatContent.innerHTML = chatContent.innerHTML.replace(typingMessage.outerHTML, botResponse);
+
+    document.getElementById("userInput").value = "";
+    chatContent.scrollTop = chatContent.scrollHeight;
+  }, 10000); // 10 seconds
+}
